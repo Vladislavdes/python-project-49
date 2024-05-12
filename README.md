10baf15af182712c0e493853b438580e7f08689d
Vladislavdes <bograb1337@gmail.com>
2024-05-12 17:00:44 +0300

    4 Commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/Vladislavdes/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Vladislavdes/python-project-49/actions)
 
 <a href="https://codeclimate.com/github/Vladislavdes/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/12919bc3f99eff84b809/maintainability" /></a>
+
+<a href='https://asciinema.org/connect/50b5bb55-d96c-4f51-8758-68c3c5ef2c41'>Brain-even</a>
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,29 @@
+import prompt
+import random
+
+def brain_even():
+    counter = 0
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while counter < 3:
+        num = random.randint(1,101)
+        print(f'Question: {num}')
+        your_answer = prompt.string('Your answer:')
+        if num % 2 == 0 and your_answer == 'yes':
+            print('correct')
+            counter += 1
+            print(counter)
+        elif num % 2 != 0 and your_answer == 'no':
+            print('correct')
+            counter += 1
+            print(counter)
+        else:
+            return f"""'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!"""
+            
+    if counter == 3:
+        print(f'Congratulations, {name}!')
+        
+
+print(brain_even())
